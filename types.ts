837af4d90fb23c401fe00c0d2a0c86e5dda33936
Fastop06 <mussa.gurov06@gmail.com>
2026-01-31 04:54:41 -0800

    Add ADMIN role and isAdmin property to User interface

--- types.ts
+++ types.ts
@@ -19,9 +19,11 @@ export enum PageView {
   SHOP = 'SHOP',
   PRODUCT_DETAIL = 'PRODUCT_DETAIL',
   CART = 'CART',
+  ADMIN = 'ADMIN',
 }
 
 export interface User {
   name: string;
   email: string;
+  isAdmin?: boolean;
 }
